2f71ef1d28c6c2fceb7d1bb04f0fbc8a873869e7
totozi <skgydnjs@gmail.com>
2023-09-25 22:23:46 +0900

    ch1-1 commit from ubuntu

--- src/ch1/Ex001.java
+++ src/ch1/Ex001.java
@@ -1,8 +1,9 @@
 package ch1;
 
-import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 
 public class Ex001 {
@@ -12,9 +13,36 @@ public class Ex001 {
         // 001. 문자 개수 세기
         // 주어진 문자열에서 문자 개수를 세는 프로그램을 작성하라.
         // aaabb이면 a 3개, b 2개
+        String param = "aaabbcccaa";
+
+        Map resultMap; 
+
+        resultMap = countCharInString(param);
+
+        System.out.println(resultMap);
 
 
     }
+
+    private static Map<Character, Integer> countCharInString(String param) {
+
+        char[] chArr = param.toCharArray();
+
+        Map<Character, Integer> map = new HashMap<Character, Integer>();
+
+        for(char ch : chArr) {
+            if(map.get(ch) != null) {
+                map.put(ch, map.get(ch) + 1);
+            } else {
+                map.put(ch, 1);
+            }
+        }
+        
+
+        return map;
+    }
+
+
 
 
 
